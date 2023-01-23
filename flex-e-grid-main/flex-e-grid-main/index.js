365eaae925f48bdf19da0bc36ac2bef0f3ae5410
joaoprabelomf <joaoprabelomf@gmail.com>
2023-01-23 19:04:07 -0300

    Mudancas

---
+++ flex-e-grid-main/flex-e-grid-main/index.js
@@ -0,0 +1,7 @@
+const botaoMenu = document.querySelector('.cabecalho-menu');
+const menu = document.querySelector('.menu-lateral');
+
+
+botaoMenu.addEventListener('click', () =>{
+    menu.classList.toggle('menu-lateral-ativo')
+})
